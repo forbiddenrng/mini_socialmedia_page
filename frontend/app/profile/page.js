5a4137a499ae0af3846e75bace3489e1f11145d2
Antoni Gawron <antek.gawron@gmail.com>
2025-01-02 16:18:02 +0100

    user settings interface to proile

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -4,6 +4,7 @@ import "../style/profile.css"
 import { MdEdit } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 import { MdOutlineCancel } from "react-icons/md";
+import { BiShow, BiHide } from "react-icons/bi";
 import { useState, useEffect } from "react";
 
 
@@ -160,7 +161,115 @@ export default function Profile() {
         </div>
 
       </div>
-      </div>   
+      </div> 
+      <UserSettings/>  
     </div>
   );
+}
+
+
+function UserSettings(){
+  //state for form management
+  const [editEmail, setEditEmail] = useState(false)
+  const [emailValue, setEmailValue] = useState('')
+
+  const [editPassword, setEditPassword] = useState(false)
+  const [passwordValue, setPasswordValue] = useState('')
+  const [passwordInputType, setPasswordInputType] = useState('password')
+  const [passwordVisible, setPasswordVisible] = useState(false)
+
+  //state for token 
+  const [token, setToken] = useState()
+
+  //state for user data
+  const [email, setEmail] = useState('Brak danych')
+  const [password, setPassword] = useState('Brak danych')
+
+  useEffect(() => {
+    if (typeof window !== 'undefined'){
+      setToken(sessionStorage.getItem('token'))
+    }
+  }, [])
+
+  const getUserInfo = async () => {
+    const response = fetch('http://localhost:4000/api/user/info', {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      },
+      credentials: "include"
+    }).then(res => res.json())
+    .then(res => {
+      saveUserInfo(res)
+    }).catch(e => {
+      alert("Nie udało się załadować danych")
+    })
+  }
+
+  const saveUserInfo = ({userData}) => {
+    setEmail(userData.email)
+    setPassword(userData.password)
+  }
+
+
+  useEffect(() => {
+    if(token){
+      getUserInfo()
+    }
+  }, [token])
+  
+
+
+  const handleConfirmEdit = (edit) => {
+    if(edit==='email'){
+      setEditEmail(false)
+    }else if(edit==='password'){
+      setEditPassword(false)
+    }
+  }
+
+  const changeInputType = () => {
+    const newType = passwordInputType === 'password' ? 'text' : 'password'
+    setPasswordInputType(newType)
+  }
+
+  const getPassword = () => passwordVisible ? password : "*".repeat(password.length)
+  return(
+    <div className="user_info">
+      <h3>Ustawienia konta</h3>
+      <div className="info_box">
+        <div className="info_title">Email: </div>
+        <div className="info_content">
+        {editEmail ? <input className="edit_input" onChange={(e) => setEmailValue(e.target.value)} value={emailValue}></input> : <span>{email}</span>}
+        </div>
+        <div className="edit_btn">
+          {!editEmail ? <button onClick={() => setEditEmail(true)}><MdEdit/></button> : 
+          <>
+            <button onClick={() => handleConfirmEdit('email')}><FaCheck/></button>
+            <button onClick={() => setEditEmail(false)}><MdOutlineCancel/></button>
+          </>
+          }
+        </div>
+      </div>
+      <div className="info_box">
+        <div className="info_title">Hasło: </div>
+        <div className="info_content">
+        {editPassword ? <input type={passwordInputType} className="edit_input" onChange={(e) => setPasswordValue(e.target.value)} value={passwordValue}></input> : <span>{getPassword()}</span>}
+        </div>
+        <div className="edit_btn">
+          {!editPassword ? <>
+            <button onClick={() => setEditPassword(true)}><MdEdit/></button>
+            <button onClick={() => setPasswordVisible(!passwordVisible)}>{passwordVisible ? <BiHide/> : <BiShow/>}</button>
+          </> : 
+          <>
+            <button onClick={() => handleConfirmEdit('password')}><FaCheck/></button>
+            <button onClick={() => setEditPassword(false)}><MdOutlineCancel/></button>
+            <button onClick={() => changeInputType()}>{passwordInputType === 'text' ? <BiHide/> : <BiShow/>}</button>
+          </>
+          }
+        </div>
+      </div>
+      
+    </div>
+  )
 }
